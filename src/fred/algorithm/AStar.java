04da4a41131c793770f40bd13e88af1bbf7b2b74
Frederick <frederick_widjaja@hotmail.com>
2012-06-26 10:14:07 +0700

    'src'

--- src/fred/algorithm/AStar.java
+++ src/fred/algorithm/AStar.java
@@ -6,175 +6,182 @@ import java.util.HashSet;
 import java.util.Set;
 
 import fred.Algorithm;
-import fred.PathFinder;
 import fred.Node;
+import fred.PathFinder;
 import fred.struct.BinaryHeap;
 
 public class AStar implements Algorithm {
 
-    public static final Heuristic MANHATTAN_DISTANCE = new ManhattanDistance();
-    public static final Heuristic CHEBYSHEV_DISTANCE = new ChebyshevDistance();
-    public static final Heuristic EUCLIDEAN_DISTANCE = new EuclideanDistance();
+	public static final Heuristic MANHATTAN_DISTANCE = new ManhattanDistance();
+	public static final Heuristic CHEBYSHEV_DISTANCE = new ChebyshevDistance();
+	public static final Heuristic EUCLIDEAN_DISTANCE = new EuclideanDistance();
 
-    private final Heuristic heuristic;
-    private final BinaryHeap<AStarNode> openQueue = new BinaryHeap<AStarNode>();
-    private final Set<AStarNode> openSet = Collections.synchronizedSet(new HashSet<AStarNode>());
-    private final Set<AStarNode> closedSet = Collections.synchronizedSet(new HashSet<AStarNode>());
-    private AStarNode current = null;
+	private final Heuristic heuristic;
 
-    public AStar(Heuristic heuristic) {
-        this.heuristic = heuristic;
-    }
+	private final BinaryHeap<AStarNode> openQueue = new BinaryHeap<AStarNode>();
+	private final Set<AStarNode> openSet = Collections
+			.synchronizedSet(new HashSet<AStarNode>());
+	private final Set<AStarNode> closedSet = Collections
+			.synchronizedSet(new HashSet<AStarNode>());
 
-    @Override
-    public void init() {
-        AStarNode source = new AStarNode(PathFinder.getSource());
-        openQueue.add(source);
-        openSet.add(source);
-    }
+	private AStarNode current = null;
 
-    @Override
-    public boolean step() {
-        if (!openQueue.isEmpty()) {
-            current = openQueue.poll();
-            openSet.remove(current);
-            closedSet.add(current);
-            if (current.equals(PathFinder.getTarget())) {
-                return true;
-            }
-            for (final AStarNode next : PathFinder.getTraversable(current)) {
-                if (!closedSet.contains(next)) {
-                    final double dist = current.distance + distance(current, next);
-                    if (!openSet.contains(next)) {
-                        next.parent = current;
-                        next.distance = dist;
-                        next.cost = dist + heuristic.calculate(next);
-                        openQueue.add(next);
-                        openSet.add(next);
-                    } else {
-                        final AStarNode old = getNode(openSet, next);
-                        if (dist < old.distance) {
-                            old.parent = current;
-                            old.distance = dist;
-                            old.cost = dist + heuristic.calculate(next);
-                            openQueue.resort(old);
-                        }
-                    }
-                }
-            }
-            return false;
-        } else {
-            current = null;
-            return true;
-        }
-    }
+	public AStar(Heuristic heuristic) {
+		this.heuristic = heuristic;
+	}
 
-    private double distance(final Node start, final Node end) {
-        if (start.x != end.x && start.y != end.y) {
-            return 1.414213562373;
-        } else {
-            return 1.0;
-        }
-    }
+	@Override
+	public void init() {
+		AStarNode source = new AStarNode(PathFinder.getSource());
+		openQueue.add(source);
+		openSet.add(source);
+	}
 
-    private AStarNode getNode(Set<AStarNode> nodes, AStarNode n) {
-        for (AStarNode node : nodes) {
-            if (node.equals(n)) {
-                return node;
-            }
-        }
-        return null;
-    }
+	@Override
+	public boolean step() {
+		if (!openQueue.isEmpty()) {
+			current = openQueue.poll();
+			openSet.remove(current);
+			closedSet.add(current);
+			if (current.equals(PathFinder.getTarget())) {
+				return true;
+			}
+			for (final AStarNode next : PathFinder.getTraversable(current)) {
+				if (!closedSet.contains(next)) {
+					final double dist = current.distance
+							+ distance(current, next);
+					if (!openSet.contains(next)) {
+						next.parent = current;
+						next.distance = dist;
+						next.cost = dist + heuristic.calculate(next);
+						openQueue.add(next);
+						openSet.add(next);
+					} else {
+						final AStarNode old = getNode(openSet, next);
+						if (dist < old.distance) {
+							old.parent = current;
+							old.distance = dist;
+							old.cost = dist + heuristic.calculate(next);
+							openQueue.resort(old);
+						}
+					}
+				}
+			}
+			return false;
+		} else {
+			current = null;
+			return true;
+		}
+	}
 
-    @Override
-    public Collection<AStarNode> getUnvisitedNodes() {
-        return openSet;
-    }
+	private double distance(final Node start, final Node end) {
+		if (start.x != end.x && start.y != end.y) {
+			return 1.414213562373;
+		} else {
+			return 1.0;
+		}
+	}
 
-    @Override
-    public Collection<AStarNode> getVisitedNodes() {
-        return closedSet;
-    }
+	private AStarNode getNode(Set<AStarNode> nodes, AStarNode n) {
+		for (AStarNode node : nodes) {
+			if (node.equals(n)) {
+				return node;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public Node getCurrentNode() {
-        return current;
-    }
+	@Override
+	public Collection<AStarNode> getUnvisitedNodes() {
+		return openSet;
+	}
 
-    @Override
-    public void reset() {
-        openQueue.clear();
-        openSet.clear();
-        closedSet.clear();
-        current = null;
-    }
+	@Override
+	public Collection<AStarNode> getVisitedNodes() {
+		return closedSet;
+	}
 
-    private class AStarNode extends Node {
+	@Override
+	public Node getCurrentNode() {
+		return current;
+	}
 
-        double cost = 0.0;
+	@Override
+	public void reset() {
+		openQueue.clear();
+		openSet.clear();
+		closedSet.clear();
+		current = null;
+	}
 
-        public AStarNode(int x, int y) {
-            super(x, y);
-        }
+	private class AStarNode extends Node {
 
-        public AStarNode(Node node) {
-            super(node);
-        }
+		double cost = 0.0;
 
-        @Override
-        public AStarNode newNode(int x, int y) {
-            return new AStarNode(x, y);
-        }
+		public AStarNode(int x, int y) {
+			super(x, y);
+		}
 
-        @Override
-        public int compareTo(Node node) {
-            double difference = cost - ((AStarNode) node).cost;
-            if (difference < 0) {
-                return -1;
-            } else if (difference > 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
+		public AStarNode(Node node) {
+			super(node);
+		}
 
-    }
+		@Override
+		public AStarNode newNode(int x, int y) {
+			return new AStarNode(x, y);
+		}
 
-    public static abstract class Heuristic {
+		@Override
+		public int compareTo(Node node) {
+			double difference = cost - ((AStarNode) node).cost;
+			if (difference < 0) {
+				return -1;
+			} else if (difference > 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 
-        public final double calculate(Node current) {
-            return calculate(current, PathFinder.getTarget());
-        }
+	}
 
-        protected abstract double calculate(Node current, Node target);
+	public static abstract class Heuristic {
 
-    }
+		public final double calculate(Node current) {
+			return calculate(current, PathFinder.getTarget());
+		}
 
-    private static class ManhattanDistance extends Heuristic {
+		protected abstract double calculate(Node current, Node target);
 
-        @Override
-        protected double calculate(Node current, Node target) {
-            return Math.abs(current.x - target.x) + Math.abs(current.y - target.y);
-        }
+	}
 
-    }
+	private static class ManhattanDistance extends Heuristic {
 
-    private static class ChebyshevDistance extends Heuristic {
+		@Override
+		protected double calculate(Node current, Node target) {
+			return Math.abs(current.x - target.x)
+					+ Math.abs(current.y - target.y);
+		}
 
-        @Override
-        protected double calculate(Node current, Node target) {
-            return Math.max(Math.abs(current.x - target.x), Math.abs(current.y - target.y));
-        }
+	}
 
-    }
+	private static class ChebyshevDistance extends Heuristic {
 
-    private static class EuclideanDistance extends Heuristic {
+		@Override
+		protected double calculate(Node current, Node target) {
+			return Math.max(Math.abs(current.x - target.x),
+					Math.abs(current.y - target.y));
+		}
 
-        @Override
-        protected double calculate(Node current, Node target) {
-            return Math.hypot(current.x - target.x, current.y - target.y);
-        }
+	}
 
-    }
+	private static class EuclideanDistance extends Heuristic {
+
+		@Override
+		protected double calculate(Node current, Node target) {
+			return Math.hypot(current.x - target.x, current.y - target.y);
+		}
+
+	}
 
 }
--- src/fred/algorithm/Dijkstra.java
+++ src/fred/algorithm/Dijkstra.java
@@ -7,56 +7,99 @@ import java.util.Set;
 
 import fred.Algorithm;
 import fred.Node;
+import fred.PathFinder;
 import fred.struct.BinaryHeap;
 
 public class Dijkstra implements Algorithm {
 
-    private final BinaryHeap<Node> unvisitedQueue = new BinaryHeap<Node>();
-    private final Set<Node> unvisitedSet = Collections.synchronizedSet(new HashSet<Node>());
-    private final Set<Node> visitedSet = Collections.synchronizedSet(new HashSet<Node>());
+	private final BinaryHeap<Node> unvisitedQueue = new BinaryHeap<Node>();
+	private final Set<Node> unvisitedSet = Collections
+			.synchronizedSet(new HashSet<Node>());
+	private final Set<Node> visitedSet = Collections
+			.synchronizedSet(new HashSet<Node>());
 
-    @Override
-    public void init() {
-        // TODO Auto-generated method stub
+	private Node current;
 
-    }
+	@Override
+	public void init() {
+		Node source = new Node(PathFinder.getSource());
+		unvisitedQueue.add(source);
+		unvisitedSet.add(source);
+	}
 
-    @Override
-    public boolean step() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean step() {
+		if (!unvisitedQueue.isEmpty()) {
+			current = unvisitedQueue.poll();
+			unvisitedSet.remove(current);
+			visitedSet.add(current);
+			if (current.equals(PathFinder.getTarget())) {
+				return true;
+			}
+			for (final Node next : PathFinder.getTraversable(current)) {
+				if (!visitedSet.contains(next)) {
+					final double dist = current.distance
+							+ distance(current, next);
+					if (!unvisitedSet.contains(next)) {
+						next.parent = current;
+						next.distance = dist;
+						unvisitedQueue.add(next);
+						unvisitedSet.add(next);
+					} else {
+						final Node old = getNode(unvisitedSet, next);
+						if (dist < old.distance) {
+							old.parent = current;
+							old.distance = dist;
+							unvisitedQueue.resort(old);
+						}
+					}
+				}
+			}
+			return false;
+		} else {
+			current = null;
+			return true;
+		}
+	}
 
-    private double distance(final Node start, final Node end) {
-        if (start.x != end.x && start.y != end.y) {
-            return 1.414213562373;
-        } else {
-            return 1.0;
-        }
-    }
+	private double distance(final Node start, final Node end) {
+		if (start.x != end.x && start.y != end.y) {
+			return 1.414213562373;
+		} else {
+			return 1.0;
+		}
+	}
 
-    @Override
-    public Collection<? extends Node> getUnvisitedNodes() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private Node getNode(Set<Node> nodes, Node n) {
+		for (Node node : nodes) {
+			if (node.equals(n)) {
+				return node;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public Collection<? extends Node> getVisitedNodes() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Collection<? extends Node> getUnvisitedNodes() {
+		return unvisitedSet;
+	}
 
-    @Override
-    public Node getCurrentNode() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Collection<? extends Node> getVisitedNodes() {
+		return visitedSet;
+	}
 
-    @Override
-    public void reset() {
-        // TODO Auto-generated method stub
+	@Override
+	public Node getCurrentNode() {
+		return current;
+	}
 
-    }
+	@Override
+	public void reset() {
+		unvisitedQueue.clear();
+		unvisitedSet.clear();
+		visitedSet.clear();
+		current = null;
+	}
 
 }
